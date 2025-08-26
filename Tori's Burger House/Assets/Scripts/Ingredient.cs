45b33179cd55d168227c92c5fd6c94add1bf62a1
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-08-26 20:41:29 +0900

    Feat Ingredient Script

--- Tori's Burger House/Assets/Scripts/Ingredient.cs
+++ Tori's Burger House/Assets/Scripts/Ingredient.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public GameObject ingredientPrefab;
+
+    public GameObject currentIngredient;
+
+    private void OnMouseDown()
     {
-        
+        if (ingredientPrefab != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            currentIngredient = Instantiate(ingredientPrefab, mousePos, Quaternion.identity);
+
+            Drag drag = currentIngredient.GetComponent<Drag>();
+            if (drag != null)
+            {
+                drag.isDragging = true;
+                drag.isDragged = false;
+
+                drag.offset = (Vector2)currentIngredient.transform.position - mousePos;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDrag()
     {
-        
+        if (currentIngredient != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            currentIngredient.transform.position = (Vector2)mousePos
+                                                   + currentIngredient.GetComponent<Drag>().offset;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (currentIngredient != null)
+        {
+            Drag drag = currentIngredient.GetComponent<Drag>();
+            if (drag != null)
+            {
+                drag.isDragging = false;
+            }
+            currentIngredient = null;
+        }
     }
 }
