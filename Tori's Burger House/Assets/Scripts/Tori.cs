8b294a5a40ca530e827def06c1218b63862e1f0d
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-08-31 02:04:42 +0900

    Feat Tori Animation

--- Tori's Burger House/Assets/Scripts/Tori.cs
+++ Tori's Burger House/Assets/Scripts/Tori.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class Tori : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Tori Instance { get; private set; }
+
+    private Animator animator;
+
+    public bool isNormal = true;
+    public bool isHappy = false;
+    public bool isAngry = false;
+
+    public enum CharacterState
     {
-        
+        Normal = 0,
+        Happy = 1,
+        Angry = 2
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        SetState(CharacterState.Normal);
+    }
+
+    private void Update()
+    {
+        // Q 키를 누르면 Normal 상태로 변경
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SetState(CharacterState.Normal);
+        }
+        // W 키를 누르면 Happy 상태로 변경
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            SetState(CharacterState.Happy);
+        }
+        // E 키를 누르면 Angry 상태로 변경
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            SetState(CharacterState.Angry);
+        }
+    }
+
+    public void SetState(CharacterState newState)
+    {
+        if (animator != null)
+        {
+            animator.SetFloat("state", (int)newState);
+            Debug.Log("현재 상태: " + newState.ToString());
+        }
     }
 }
