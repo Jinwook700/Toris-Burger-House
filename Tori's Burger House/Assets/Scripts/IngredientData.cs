9afcca77f756552d0340d5bde90a9d65d67612f7
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-08-31 05:44:23 +0900

    Update Prefab

--- Tori's Burger House/Assets/Scripts/IngredientData.cs
+++ Tori's Burger House/Assets/Scripts/IngredientData.cs
@@ -5,5 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "IngredientData", menuName = "Scriptable Objects/Ingredient Data")]
 public class IngredientData : ScriptableObject
 {
+    public enum IngredientType
+    {
+        Bun = 0,
+        Tomato = 1,
+        Lettuce = 2,
+        Onion = 3,
+        Pickle = 4,
+        Meat = 5,
+        Cheese = 6,
+        FriedPotato = 7,
+        Cola = 8,
+        Cider = 9,
+        OrangeJuice = 10,
+    }
+
+    [Header("Ingredients")]
     public List<GameObject> ingredientPrefabs;
 }
