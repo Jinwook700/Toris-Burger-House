919cd19bc872254ad0d6740c9359cc0e79a71569
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-09-04 19:14:34 +0900

    Update Clear/NotClear Sound

--- Tori's Burger House/Assets/Scripts/Sound/BgmPlay.cs
+++ Tori's Burger House/Assets/Scripts/Sound/BgmPlay.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Sound;
 using UnityEngine;
+using UnityEngine.Apple.ReplayKit;
 
 public class BgmPlay : MonoBehaviour
 {
     public string bgmName;
+    public bool replay = true;
     private void Start()
     {
         SoundManager.Instance.StopAllBgm();
         SoundObject _soundObject;
-        _soundObject = Sound.Play(bgmName, true);
+        _soundObject = Sound.Play(bgmName, replay);
     }
 }
