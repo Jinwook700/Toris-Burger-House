559579e2e3ea3955f6498008231e574221f1e2d6
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-09-04 19:22:22 +0900

    Feat GameStart Sound

--- Tori's Burger House/Assets/Scripts/Ui/GameStart.cs
+++ Tori's Burger House/Assets/Scripts/Ui/GameStart.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Sound;
+using UnityEngine.UIElements;
 
 public class GameStart : MonoBehaviour
 {
     [SerializeField]
     string sceneName;
+    public string soundName;
     public void OnButtonClick()
     {
+        if (soundName != null)
+        {
+            SoundObject _soundObject;
+            _soundObject = Sound.Play(soundName, false);
+            _soundObject.SetVolume(0.9f);
+        }
         SceneManager.LoadScene(sceneName);
     }
 }
