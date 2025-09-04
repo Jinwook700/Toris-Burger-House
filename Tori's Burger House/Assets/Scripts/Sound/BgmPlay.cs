d9916ad986442e5b19a57ba98d066133736f02fe
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-09-04 19:17:01 +0900

    Update Set Volume

--- Tori's Burger House/Assets/Scripts/Sound/BgmPlay.cs
+++ Tori's Burger House/Assets/Scripts/Sound/BgmPlay.cs
@@ -8,10 +8,12 @@ public class BgmPlay : MonoBehaviour
 {
     public string bgmName;
     public bool replay = true;
+    public float setVol = 0.1f;
     private void Start()
     {
         SoundManager.Instance.StopAllBgm();
         SoundObject _soundObject;
         _soundObject = Sound.Play(bgmName, replay);
+        _soundObject.SetVolume(setVol);
     }
 }
