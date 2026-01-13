1a5dc2b24eb129ba40dacfd53c9b6b107d6ab18e
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2026-01-13 20:41:55 +0900

    ReFactoring ResultUI Script

--- Tori's Burger House/Assets/Scripts/Ui/ResultUI.cs
+++ Tori's Burger House/Assets/Scripts/Ui/ResultUI.cs
@@ -1,6 +1,9 @@
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// 게임 결과 시 Gold 표시
+/// </summary>
 public class ResultUI : MonoBehaviour
 {
     [SerializeField] private TextMeshPro resultGoldText;
